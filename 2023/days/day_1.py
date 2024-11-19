3f21a280f37955d8d31204220d85a26eec7912e5
René Moll <github@r-moll.nl>
2024-11-19 22:05:18 +0100

    add precommit

--- 2023/days/day_1.py
+++ 2023/days/day_1.py
@@ -1,8 +1,12 @@
 from aocd import get_data
 
+NAMES = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
 def _extract_numbers_from_line(line):
     numbers = [x for x in line if x.isdigit()]
     return int(numbers[0] + numbers[-1])
+
 
 def _part1(input_lines):
     result = 0
@@ -10,22 +14,25 @@ def _part1(input_lines):
         result += _extract_numbers_from_line(line)
     return result
 
-NAMES = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 def _strip_line_to_first_last_number(line):
-    while not any(filter(lambda x: line.startswith(x), NAMES)) and not line[0].isdigit():
+    while (
+        not any(filter(lambda x: line.startswith(x), NAMES)) and not line[0].isdigit()
+    ):
         line = line[1:]
     while not any(filter(lambda x: line.endswith(x), NAMES)) and not line[-1].isdigit():
         line = line[:-1]
     return line
 
+
 def _replace_textual_with_digits(line):
-    for i,name in enumerate(NAMES):
+    for i, name in enumerate(NAMES):
         if line.startswith(name) and not line[0].isdigit():
-            line = line.replace(name, str(i+1), 1)
+            line = line.replace(name, str(i + 1), 1)
         if line.endswith(name) and not line[-1].isdigit():
-            line = (str(i+1)).join(line.rsplit(name, 1))
+            line = (str(i + 1)).join(line.rsplit(name, 1))
     return line
+
 
 def _part2(input_lines):
     result = 0
@@ -35,10 +42,19 @@ def _part2(input_lines):
         result += _extract_numbers_from_line(line)
     return result
 
+
 def solve(input_lines):
     return (_part1(input_lines), _part2(input_lines))
 
+
 if __name__ == "__main__":
-    data = get_data(day=1, year=2023).splitlines()
-    print(f"Part 1: {_part1(data)}")
-    print(f"Part 2: {_part2(data)}")
+    from aocd.models import Puzzle
+
+    puzzle = Puzzle(year=2023, day=1)
+    example = puzzle.examples[0]
+    example_input = example.input_data.splitlines()
+    print(f"Part 1: {_part1(example_input)}, expecting: {example.answer_a}")
+
+    example = puzzle.examples[1]
+    example_input = example.input_data.splitlines()
+    print(f"Part 2: {_part2(example_input)}, expecting: {example.answer_b}")
