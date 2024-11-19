4e16e6a388d7ec2f2531ac7b681a46dde3cd4e19
René Moll <github@r-moll.nl>
2024-11-19 17:00:03 +0100

    2023: add day 1

---
+++ 2023/run.py
@@ -0,0 +1,31 @@
+import logging
+import pkgutil
+import pathlib
+import importlib
+
+from aocd import get_data
+
+
+def main() -> int:
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s: %(message)s",
+        datefmt="%Y.%m.%d %H:%M:%S",
+    )
+    
+    tasks_path = pathlib.Path(__file__).parent.resolve() / "days"
+    modules = [
+        name for _, name, _ in pkgutil.iter_modules([str(tasks_path)])
+    ]
+    logging.debug("Found the following modules: %s", modules)
+
+    for day in modules:
+        day_number = int(day.split("_")[1])
+        data = get_data(day=day_number, year=2023).splitlines()
+
+        module = importlib.import_module(f"days.{day}")
+        solution = module.solve(data)
+        print(f"Day {day_number}, part 1: {solution[0]}, part 2: {solution[1]}")
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# Setup
+
+1. Set the AoC session id: [How to get the session cookie](https://github.com/wimglenn/advent-of-code-wim/issues/1)
