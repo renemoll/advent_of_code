5a22859a04be7324f2150fd9bad8747dbda90800
René Moll <github@r-moll.nl>
2024-11-19 22:05:18 +0100

    add precommit

--- 2023/days/day_2.py
+++ 2023/days/day_2.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 
+
 def _line_to_game(line):
     game, draws = line.split(":")
     id = int(game[4:])
@@ -7,23 +8,21 @@ def _line_to_game(line):
     result = []
     for draw in draws.split(";"):
         dice = draw.split(",")
-        r = {'red': 0, 'green': 0, 'blue': 0}
+        r = {"red": 0, "green": 0, "blue": 0}
         for d in dice:
             n, c = d.strip().split(" ")
             r[c] = int(n)
         result.append(r)
     return (id, result)
 
+
 def _game_within_limit(game):
-    limit = {
-        'red': 12,
-        'green': 13,
-        'blue': 14
-    }
-    for c,v in game.items():
+    limit = {"red": 12, "green": 13, "blue": 14}
+    for c, v in game.items():
         if v > limit[c]:
             return False
     return True
+
 
 def _part1(input_lines):
     result = 0
@@ -33,22 +32,27 @@ def _part1(input_lines):
             result += game[0]
     return result
 
+
 def _part2(input_lines):
     result = 0
     for line in input_lines:
         game = _line_to_game(line)
-        r = max([x['red'] for x in game[1]])
-        g = max([x['green'] for x in game[1]])
-        b = max([x['blue'] for x in game[1]])
-        result += r*g*b
+        r = max([x["red"] for x in game[1]])
+        g = max([x["green"] for x in game[1]])
+        b = max([x["blue"] for x in game[1]])
+        result += r * g * b
     return result
+
 
 def solve(input_lines):
     return (_part1(input_lines), _part2(input_lines))
 
+
 if __name__ == "__main__":
     from aocd.models import Puzzle
+
     puzzle = Puzzle(year=2023, day=2)
-    data = puzzle.examples
-    print(f"Part 1: {_part1(data[0].input_data.splitlines())}, expecting: {data[0].answer_a}")
-    print(f"Part 2: {_part2(data[0].input_data.splitlines())}, expecting: {data[0].answer_b}")
+    example = puzzle.examples[0]
+    example_input = example.input_data.splitlines()
+    print(f"Part 1: {_part1(example_input)}, expecting: {example.answer_a}")
+    print(f"Part 2: {_part2(example_input)}, expecting: {example.answer_b}")
--- 2023/run.py
+++ 2023/run.py
@@ -6,17 +6,15 @@ import importlib
 from aocd import get_data
 
 
-def main() -> int:
+def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s: %(message)s",
         datefmt="%Y.%m.%d %H:%M:%S",
     )
-    
+
     tasks_path = pathlib.Path(__file__).parent.resolve() / "days"
-    modules = [
-        name for _, name, _ in pkgutil.iter_modules([str(tasks_path)])
-    ]
+    modules = [name for _, name, _ in pkgutil.iter_modules([str(tasks_path)])]
     logging.debug("Found the following modules: %s", modules)
 
     for day in modules:
@@ -26,6 +24,7 @@ def main() -> int:
         module = importlib.import_module(f"days.{day}")
         solution = module.solve(data)
         print(f"Day {day_number}, part 1: {solution[0]}, part 2: {solution[1]}")
+
 
 if __name__ == "__main__":
     main()
