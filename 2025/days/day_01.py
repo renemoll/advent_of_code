d5f90a3cfbb00f0032e4fee1e1625fdc02c60826
René Moll <github@r-moll.nl>
2025-12-02 20:35:48 +0100

    2025: add days 1 and 2

---
+++ 2025/days/day_01.py
@@ -0,0 +1,62 @@
+"""Day 1: Secret Entrance"""
+
+from collections.abc import Iterator
+import math
+
+
+def _parse(input_data: str):
+    rotations = [
+        int(line[1:]) * (1 if line[0] == "R" else -1)
+        for line in input_data.splitlines()
+    ]
+    return rotations
+
+
+def range_limited_sum(data: list[int]) -> Iterator[int]:
+    """Yields the cumulative sum of data, wrapped around at 100."""
+    maximum = 100
+    result = 50
+    for x in data:
+        result = (result + x) % maximum
+        yield result
+
+
+def _part1(parsed_input) -> int:
+    rotation = range_limited_sum(parsed_input)
+    return sum(map(lambda x: 1 if x == 0 else 0, rotation))
+
+
+def range_limited_sum_with_crossings(data: list[int]) -> Iterator[int]:
+    """Yields the number of times 0 is crossed for each rotation in data."""
+    maximum = 100
+    result = 50
+    for x in data:
+        crossings = 0
+        for _ in range(abs(x)):
+            s = math.copysign(1, x)
+            result = (result + s) % maximum
+            crossings += 1 if result == 0 else 0
+        yield crossings
+
+
+def _part2(parsed_input) -> int:
+    rotation = range_limited_sum_with_crossings(parsed_input)
+    return sum(rotation)
+
+
+def solve(input_data: str) -> tuple[int, int]:
+    """Solve both parts of the puzzle."""
+    parsed_input = _parse(input_data)
+    return (_part1(parsed_input), _part2(parsed_input))
+
+
+if __name__ == "__main__":
+    from aocd.models import Puzzle
+
+    puzzle = Puzzle(year=2025, day=1)
+    example = puzzle.examples[0]
+    example_input = example.input_data
+
+    solution = solve(example_input)
+    print(f"Part 1: {solution[0]}, expecting: {example.answer_a}")
+    print(f"Part 2: {solution[1]}, expecting: {example.answer_b}")
